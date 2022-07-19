7f4b82d1b9a1bc542ae6b1d96329bc3015ef704a
PRATIK <pp757439@gmail.com>
2022-07-19 12:54:15 +0530

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+        <img src="./logo512.png" alt="" width="70" height="55"/>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <button className="btn btn-outline-success" type="submit">Sign Up</button>
+      </form>
+    </div>
+  </div>
+</nav>
+     
+    </div>
+  )
+}
