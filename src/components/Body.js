12cdbfd9ffb2d06f1bd03506f877c7aa9968f95e
PRATIK <pp757439@gmail.com>
2022-08-08 21:03:01 +0530

    commit

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Body.css"
+
+export default function Body() {
+  return (
+    <div class="first">
+      <h3>Trusted By </h3>
+      <div class="second">
+        
+      </div>
+    </div>
+  )
+}
