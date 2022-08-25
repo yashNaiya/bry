d5dd0848174e6d5178ab83406a5a7c7d9ce2fb49
PRATIK <pp757439@gmail.com>
2022-08-25 18:40:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import ABC from './components/ABC';
-import About from './components/About';
-import Body from './components/Body';
-
 function App() {
   return (
-    <>
-   <Navbar/>
-   <ABC/>
-   <Body/>
-   <About/>
-    </>
+  <></>
   );
 }
 
-export default App;
+export default App
