b9cd00e77181649cb6087e99ac8c1261e4670f8e
yashNaiya <yashnaiya16@gmail.com>
2022-09-10 13:33:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import { Box } from "@mui/material"
 import Landing from "./components/Landing";
 import JobMain from "./components/JobMain";
 import Authentication from "./components/Authentication";
-
 function App() {
 
   return (
-    // <Landing />
+    <Landing />
     // <JobMain/>
-    <Authentication/>   
+    // <Authentication/>   
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import theme from './theme'
+import { BrowserRouter } from "react-router-dom"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
