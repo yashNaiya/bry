dc3dc38cd81c5fb36f81c50cebaba229a79fc7ba
yashNaiya <yashnaiya16@gmail.com>
2022-09-11 12:06:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Box } from "@mui/material"
 import Landing from "./components/Landing";
 import JobMain from "./components/JobMain";
 import Authentication from "./components/Authentication";
+import { Routes, Route } from "react-router-dom"
 function App() {
 
   return (
-    <Landing />
-    // <JobMain/>
-    // <Authentication/>   
+    <Routes>
+      <Route path='/' element={<Landing/>}></Route>
+      <Route path='/jobpage' element={<JobMain/>}></Route>
+    </Routes>
   );
 }
 
 export default App;
+
+{/* <Authentication /> */}
