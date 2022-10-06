061585550eb7486545ed542c8d9ced2cd040f13a
yashNaiya <yashnaiya16@gmail.com>
2022-10-06 20:59:16 +0530

    commit

--- src/components/Job/seeker/post1.jsx
+++ src/components/Job/seeker/post1.jsx
@@ -13,22 +13,23 @@ import {  useNavigate } from 'react-router-dom';
 import PostPage from './PostPage';
 
 const Post1 = (data) => {
+    const navigate = useNavigate()
+    // console.log(data.location)
+    const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('sessionData')))
 
     const handleApply = (Job_id, user_id) => {
-        console.log(user._id)
+        console.log(user_id)
         console.log(data.user_id_In_Job)
         if (user_id === data.user_id_In_Job) {
             alert("You Can Not Apply Jobs You Posted")
             return
         }
-        else{
-        axios.post(`/ApplyForJob/${Job_id}/${user_id}`)
+        axios.post(`/ApplyForJob/${Job_id}/${user_id}`, user_id, Job_id)
             .then(response => {
                 alert(response.data.message)
             })
             .catch(err => console.log(err))
     }
-}
     const handleView = () => {
         // data.setJob()
         data.showPost(true)
@@ -61,7 +62,7 @@ const Post1 = (data) => {
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <Button onClick={()=>handleApply(data.Job_id,user._id)}>
+                <Button onClick={handleApply}>
                     Apply
                 </Button>
                 <IconButton onClick={handleView} >
